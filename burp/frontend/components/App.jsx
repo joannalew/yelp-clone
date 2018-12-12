93717093bdd3e8064767310ff63fac4a39e5a27f
Joanna <lewj@oregonstate.edu>
2018-12-12 13:56:35 -0800

    flexing those css muscles

--- burp/frontend/components/App.jsx
+++ burp/frontend/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AuthRoute } from '../util/route_util';
 import GreetingContainer from './greeting/greeting_container';
 import SignupFormContainer from './session_form/signup_form_container';
@@ -8,8 +8,20 @@ import LoginFormContainer from './session_form/login_form_container';
 const App = () => (
     <div>
         <header>
-            <h1>Buuurp</h1>
-            <GreetingContainer />
+            <div className="header-image">
+                <div className="nav-main">
+                    <div className="nav-left">
+                        <ul className="nav-content">
+                            <li className="header-item"><Link to={'/'}>Write a Review</Link></li>
+                            <li className="header-item"><Link to={'/'}>Events</Link></li>
+                            <li className="header-item"><Link to={'/'}>Talk</Link></li>
+                        </ul>
+                    </div>
+                    <div className="nav-right">
+                        <GreetingContainer />
+                    </div>
+                </div>
+            </div>
         </header>
 
         <AuthRoute exact path="/login" component={LoginFormContainer} />
--- burp/frontend/components/greeting/greeting.jsx
+++ burp/frontend/components/greeting/greeting.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout }) => {
     const sessionLinks = () => (
-        <nav className='login-signup'>
-            <Link to='/login'>Log in</Link><br/>
-            <Link to='/signup'>Sign up!</Link>
-        </nav>
+        <ul className="nav-content">
+            <li className="header-item"><Link to='/login'>Log In</Link></li>
+            <li className="header-item signup-btn"><Link to='/signup'>Sign Up</Link></li>
+        </ul>
     );
 
     const personalGreeting = () => (
