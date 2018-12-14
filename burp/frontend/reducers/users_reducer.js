e4e06f1d8ad271ade7adad586bd8a3177a128ba9
Joanna <lewj@oregonstate.edu>
2018-12-13 17:35:25 -0800

    remove console.log

--- burp/frontend/reducers/users_reducer.js
+++ burp/frontend/reducers/users_reducer.js
@@ -5,7 +5,6 @@ const usersReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     switch(action.type) {
         case RECEIVE_CURRENT_USER:
-            console.log(action);
             return merge({}, oldState, { [action.user.id]: action.user });
         default:
             return oldState;
