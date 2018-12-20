bb063cb9249ab0e3965895838708537fa0994a4f
Joanna <lewj@oregonstate.edu>
2018-12-20 01:40:50 -0800

    splash page images random

--- burp/frontend/components/greeting/home.jsx
+++ burp/frontend/components/greeting/home.jsx
@@ -3,37 +3,41 @@ import { Link } from 'react-router-dom';
 import GreetingContainer from './greeting_container';
 import SearchBarContainer from '../search/search_bar_container';
 
-const Home = () => (
-    <div>
-        <header className="main-header">
-            <div className="header-image background-tint">
-                <div className="header-content">
-                    <div className="nav-main">
-                        <div className="nav-left">
-                            <ul className="nav-content">
-                                <Link to={'/businesses'}><li className="header-item">Write a Review</li></Link>
-                                <Link to={'/'}><li className="header-item">Events</li></Link>
-                                <Link to={'/'}><li className="header-item">Talk</li></Link>
-                            </ul>
-                        </div>
-                        <div className="nav-right">
-                            <GreetingContainer path='home' />
-                        </div>
-                    </div>
+const Home = () => {
+    const headerRand = Math.floor(Math.random() * 7);
 
-                    <div className="header-main">
-                        <div className="header-logo-center">
-                            <h1 className="header-logo">
-                                <Link to={'/'}>Buurp</Link>
-                            </h1>
+    return (
+        <div>
+            <header className="main-header">
+                <div className={`header-image header-image-${headerRand} background-tint`}>
+                    <div className="header-content">
+                        <div className="nav-main">
+                            <div className="nav-left">
+                                <ul className="nav-content">
+                                    <Link to={'/businesses'}><li className="header-item">Write a Review</li></Link>
+                                    <Link to={'/'}><li className="header-item">Events</li></Link>
+                                    <Link to={'/'}><li className="header-item">Talk</li></Link>
+                                </ul>
+                            </div>
+                            <div className="nav-right">
+                                <GreetingContainer path='home' />
+                            </div>
                         </div>
 
-                        <SearchBarContainer placeholder="burgers, barbers, spas, handymen..."/>
+                        <div className="header-main">
+                            <div className="header-logo-center">
+                                <h1 className="header-logo">
+                                    <Link to={'/'}>Buurp</Link>
+                                </h1>
+                            </div>
+
+                            <SearchBarContainer placeholder="burgers, barbers, spas, handymen..."/>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </header>
-    </div>
-);
+            </header>
+        </div>
+    )
+};
 
 export default Home;
