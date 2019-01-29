bee7bbb8a332cb968c09d00977b8f8f0e6d8e650
Joanna <lewj@oregonstate.edu>
2019-01-29 15:13:14 -0800

    add footer

--- burp/frontend/components/App.jsx
+++ burp/frontend/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { AuthRoute } from '../util/route_util';
 import Home from './greeting/home';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
@@ -9,7 +9,7 @@ import BusinessShowContainer from './business_show/business_show_container';
 import CreateReviewFormContainer from './review_form/create_review_form_container';
 import EditReviewFormContainer from './review_form/edit_review_form_container';
 import BusinessSearchContainer from './business/business_search_container';
-
+import FooterComponent from './greeting/footer';
 
 const App = () => (
     <div>
@@ -21,6 +21,7 @@ const App = () => (
         <Route exact path="/biz/:businessId/review" component={CreateReviewFormContainer} />
         <Route exact path="/biz/:businessId/edit/:reviewId" component={EditReviewFormContainer} />
         <Route path="/businesses/search" component={BusinessSearchContainer} />
+        <Route path="/" component={FooterComponent} />
     </div>
 );
 
